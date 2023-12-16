58200bb1e16b49bfd7d6840849602cc1b7d500d0
Nathan Mota <nathansmota@gmail.com>
2023-12-16 08:51:47 -0300

    :lipstick: feat: add resposive and style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
     return (
         <div className="contentProfile">
             <div className="contentImg"></div>
+            <h1>Nathan Mota</h1>
         </div>
     )
 }
